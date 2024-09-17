3d485a11f805275c8399fe267adc73b8ce943f89
Your Namejonas <skonby_jonas@hotmail.com>
2024-09-17 19:35:03 +0200

    skapat variable int saldo för hålla koll på saldot på konto samt påbörjat skapa funktionen för att hålla koll på hur mkt saldo det finns på kontot

--- src/App.java
+++ src/App.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // skapa en scanner för att kunna skriva in saker i terminalen
+        Scanner input = new Scanner(System.in); 
+
+        int saldo = 0; // en variable för att hålla koll på saldot på kontot
+
+
+        // skapa funktion för att se hur mycket saldo det finns på konto 
+
+        System.out.println("Välkommen till banken! Välj ett alternativ?  n 1. Se saldo");
+        int val = input.nextInt();
+
+        
+
+        
+
+
+
     }
 }

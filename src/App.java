4c6b57fca8ed1df09b1b72fca770810b7579c217
Your Namejonas <skonby_jonas@hotmail.com>
2024-09-18 20:28:07 +0200

    Åtgärdat error i mina metoder för så att alla alternativ i min menyval skall fungera. Åtgätdat att använderna inte kan sätta eller ta ut ett belopp mindre än 0, använder mig nu av hasNextInt för att använderna inte ska kunna mata in ett annat tal än heltal i min menyval

--- src/App.java
+++ src/App.java
@@ -1,27 +1,23 @@
 import java.util.Scanner;
 
 public class App {
-    static double balance = 0; // static för att kunna använda variablen i hela klassen och inte bara i main
-                               // metoden
-    static Scanner input = new Scanner(System.in); // static för att kunna använda input i hela klassen och inte bara i
-                                                   // main metoden
+    static double balance = 0; // static för att kunna använda variablen i hela klassen och inte bara i main metoden
+    static Scanner input = new Scanner(System.in); // static för att kunna använda input i hela klassen och inte bara i main metoden
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         boolean run = true; // skapa en boolean för att kunna köra programmet i en loop sålänge run är true
 
-        while (run) {
-            // meny för att välja vad
+        while (run) { // loop för att köra programmet så länge run är true
+            // meny för att välja val
             System.out.println("Välj ett alternativ:");
             System.out.println("1. Se saldo");
             System.out.println("2. Insättning");
             System.out.println("3. Uttag");
             System.out.println("4. Avsluta");
-            int val = input.nextInt(); //
 
-            if (input.hasNextInt()) { // kolla om input är ett nummer
-                val = input.nextInt();
-
-                switch (val) {
+            if (input.hasNextInt()) { // kolla om input är ett heltal
+                int choice = input.nextInt();  // läser användarens val
+                switch (choice) { // switch case för att välja val
                     case 1:
                         getBalance();
                         break;
@@ -34,13 +30,12 @@ public class App {
                         withdraw();
                         break;
                     case 4:
-                        exit();
+                        exit(); 
                         run = false;
                         break;
 
-                    default:
-                        System.out.println("Felaktigt val. Vänligen välj ett nummer mellan 1 och 4."); // om fel val
-                                                                                                       // görs i menyn
+                    default: // om fel val görs
+                        System.out.println("Felaktigt val. Vänligen välj ett nummer mellan 1 och 4."); // om fel val görs i menyn
                         break;
                 }
 
@@ -48,12 +43,11 @@ public class App {
                 System.out.println("Felaktigt inmatning. Vänligen ange ett nummer."); // om fel inmatning görs
                 input.next(); // rensa input
             }
-            input.close(); // stänga input
         }
-
+        input.close(); // stänga input
     }
 
-    // metod för att se saldo
+    // metod för att visa saldo
     public static void getBalance() {
         System.out.println("Ditt saldo är: " + balance + " kronor.");
     }
@@ -62,30 +56,40 @@ public class App {
     public static void deposit() {
         System.out.println("Välj belopp att sätta in på kontot: ");
         if (input.hasNextDouble()) { // kolla om input är ett nummer
-            double deposit = input.nextDouble();
-            balance += deposit;
+            double deposit = input.nextDouble(); // läser in insättning
+            if (deposit > 0) {
+                balance += deposit;
+                System.out.println(deposit + " kronor har blivit insatt på ditt konto.");
+            } else {
+                System.out.println("Felaktigt inmatning. Vänligen ange ett positivt nummer.");
+            }
         } else { // om input inte är ett nummer
-            System.out.println("Felaktigt inmatning. Vänligen ange ett nummer.");
-            input.next();
+            System.out.println("Felaktigt inmatning. Vänligen ange ett belopp.");
+            input.next(); // rensa felaktig inmatning
         }
+        input.nextLine(); // rensar eventuell buffert
 
     }
 
     // metod för uttag
     public static void withdraw() {
         System.out.println("Välj belopp att ta ut från kontot: ");
-        if (input.hasNextDouble()) {
+        if (input.hasNextDouble()) { // kolla om input är ett giltigt decimaltal
             double withdraw = input.nextDouble();
             if (withdraw > balance) { // om uttaget är större än saldot
                 System.out.println("Du har inte tillräckligt med pengar på kontot.");
-            } else {
+            } else if (withdraw <= 0) { // om uttaget är mindre än eller lika med 0
+                System.out.println("Felaktigt inmatning. Beloppet måste vara större än 0.");
+            }
+            else {
                 balance -= withdraw;
                 System.out.println(withdraw + " kronor har blivit uttaget från ditt konto.");
             }
         } else {
-            System.out.println("Felaktigt inmatning. Vänligen ange ett nummer.");
+            System.out.println("Felaktigt inmatning. Vänligen ange ett belopp.");
             input.next(); // rensa input
         }
+        input.nextLine(); // rensar eventuell buffert
     }
 
     // metod för att avsluta programmet
